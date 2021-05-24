335309b7a374572c94a40cc90a7bb67c1a79f767
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2021-05-23 17:19:44 -0700

    100% Memory!

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -1,25 +1,19 @@
+# 100% memory!
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
 
-        # init left and right pointers
-        size = len(height)
-
-        l, r = 0, size - 1
-
+        # init area
         area = 0
 
-        max_width = size - 1
+        # check each width, blasting the shorter height from existence
+        for w in range(len(height) - 1, 0, -1):
 
-        # iterate through all possible widhts
-        for width in range(max_width, 0, -1):
+            area = max(area, w * min(height[0], height[-1]))
 
-            # calculate area passed on shorter height
-            area = max(area, width * min(height[l], height[r]))
-
-            # remove shorter height from container because we've found it's max area
-            if height[l] < height[r]:
-                l += 1
+            if height[0] < height[-1]:
+                height.pop(0)
             else:
-                r -= 1
+                height.pop()
 
         return area
