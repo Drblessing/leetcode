f2848f34c41d28f382c6bc8dc41b726a991e3e80
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2021-06-19 00:01:53 -0700

    pep8

--- Merge Intervals.py
+++ Merge Intervals.py
@@ -1,15 +1,21 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x: x[0])
+
         merged = []
-        for i in intervals:
-            # if the list of merged intervals is empty
-            # or if the current interval does not overlap with the previous,
-            # simply append it.
-            if not merged or merged[-1][-1] < i[0]:
+
+        for i in sorted(intervals, key=lambda x: x[0]):
+
+            # Init answer with first interval
+            if not merged: merged.append(i)
+
+            # No collision in intervals
+            if merged[-1][-1] < i[0]:
                 merged.append(i)
-            # otherwise, there is overlap,
-            # so we merge the current and previous intervals.
+
+            # Collision in intervals
             else:
-                merged[-1][-1] = max(merged[-1][-1], i[-1])
+
+                # Update current interval with new ending
+                merged[-1][-1] = max(merged[-1][-1], i[1])
+
         return merged
