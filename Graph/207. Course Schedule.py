79b30a7259e7584ccdc9d2d93d5916f6089a432b
drblessing <52829488+Drblessing@users.noreply.github.com>
2023-01-13 20:44:40 +0000

    course schedule

--- Graph/207. Course Schedule.py
+++ Graph/207. Course Schedule.py
@@ -1,6 +1,28 @@
-class Node:
-
-    def __init__(self, )
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Topological sort BFS
+        # 1. Create graph with inDegree
+        # 2. Iterate through source nodes
+        # 3. Check if we've visited everynode
+
+        graph = defaultdict(list)
+        inDegree = [0 for _ in range(numCourses)]
+        sources = deque()
+
+        for adj, node in prerequisites:
+            graph[node].append(adj)
+            inDegree[adj] += 1
+
+        for node, degree in enumerate(inDegree):
+            if degree == 0:
+                sources.append(node)
+        visited = 0
+        while sources:
+            node = sources.popleft()
+            visited += 1
+            for adj in graph[node]:
+                inDegree[adj] -= 1
+                if inDegree[adj] == 0:
+                    sources.append(adj)
+
+        return visited == numCourses
