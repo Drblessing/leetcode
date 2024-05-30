b949565ca199a1cdd346a415a5457d5292caaae3
Danny Blessing <drblessing@gmail.com>
2024-05-30 10:56:44 -0400

    Assign cookies

--- Problems/455. Assign Cookies.py
+++ Problems/455. Assign Cookies.py
@@ -1,15 +1,55 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a console handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# Create a formatter and set the formatter for the handler
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(ch)
+
+
 class Solution:
-    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+    def findContentChildren(self, g: list[int], s: list[int]) -> int:
+        """Find the maximum number of children that can be satisfied.
+
+        g: list[int] -> List of greed factors
+        s: list[int] -> List of cookie sizes
+        return: int -> The maximum number of children that can be satisfied
+        """
+        logger.debug(f"Executing findContentChildren\nwith g: {g} and s: {s}")
+
+        # Error handling
+        if not g:
+            raise ValueError("The children list cannot be empty.")
+
+        # Sort the lists
         g.sort()
         s.sort()
 
-        satisfied_children = 0
-        while s and g:
-            smallest_cookie = s.pop(0)
-            # Check if smallest cookie can satisfy any child
-            least_greedy_child = g[0]
-            if smallest_cookie >= least_greedy_child:
-                g.pop(0)
-                satisfied_children += 1
+        # Initialize the counters
+        c = 0
+        i = 0
 
-        return satisfied_children
+        # Loop through the cookies and children
+        while i < len(s) and c < len(g):
+
+            # The current cookie can satisfy the current child
+            # We go from the least greedy child to the most greedy child
+            if s[i] >= g[c]:
+                c += 1
+
+            # Check the next cookie
+            i += 1
+
+        logger.debug(f"Returning {c}")
+        return c
+
+
+Solution().findContentChildren([1, 2, 3], [1, 1])
