23a038ef49745c5f3fa26a5e6f36892e9a6fd8d4
Danny Blessing <drblessing@gmail.com>
2024-06-27 23:48:19 -0400

    Same Tree

---
+++ Problems/100. Same Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        stack = [(p, q)]
+
+        while stack:
+            p, q = stack.pop()
+
+            if p or q:
+                if not p or not q or p.val != q.val:
+                    return False
+
+                stack.extend([(p.left, q.left), (p.right, q.right)])
+
+        return True
