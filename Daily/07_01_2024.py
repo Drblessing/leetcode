b3c23adffdebb283396fa6a2c83059b402580092
Danny Blessing <drblessing@gmail.com>
2024-07-01 20:22:40 -0400

    intersect

---
+++ Daily/07_01_2024.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return (Counter(nums1) & Counter(nums2)).elements()
