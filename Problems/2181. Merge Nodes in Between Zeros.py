c75365941d83c03059f62e3f6a919a8f7b66eff4
Danny Blessing <drblessing@gmail.com>
2024-07-05 19:28:36 -0400

    timeit

--- Problems/2181. Merge Nodes in Between Zeros.py
+++ Problems/2181. Merge Nodes in Between Zeros.py
@@ -8,25 +8,24 @@ class Solution:
         """Merge nodes between 0 on the linked list."""
 
         # Create a curNode that starts at the head
-        curNode = head
+        curNode = head 
+        sumNode = curNode.next
 
         # Iterate through the linked list
-        while curNode.next:
-            # Create a fast pointer that iterates until we reach the next 0
-            curSum = 0
-            fast = curNode.next
-            while fast.val != 0:
-                curSum += fast.val
-                fast = fast.next
-            # Set curNode to fast after changing val
+        while sumNode:
+            # Create a fast pointer that iterates until we reach the next 0 
+            curSum = 0 
+            # Iterate until next 0 
+            while sumNode.val != 0: 
+                curSum += sumNode.val
+                sumNode = sumNode.next
+            # Set value to sum 
             curNode.val = curSum
-            curNode.next = fast
-            curNode = fast
-
-        # Remove 0
-        curNode = head
-        while curNode:
-            if curNode.next.val == 0:
-                curNode.next = None
+            # Iterate to next node or end of list
+            sumNode = sumNode.next
+            # Set curNode next to this
+            curNode.next = sumNode
+            # Go to the next node
             curNode = curNode.next
+
         return head
