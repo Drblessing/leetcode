d83ea3ee0fa3d8b6e880192a33e9917c175e5e18
Danny Blessing <drblessing@gmail.com>
2024-07-14 15:20:33 -0400

    07_14

---
+++ Daily/07_14_2024.py
@@ -0,0 +1,35 @@
+class Solution:
+    @staticmethod
+    def parse_chemical_symbol(formula: str, n: int):
+        """Parse a chemical symbol and return the index
+        after the symbol and it's string value."""
+
+    @staticmethod
+    def is_digit(char: str):
+        """Check if a character is a digit."""
+        return char.isdigit()
+
+    @staticmethod
+    def is_lowercase_letter(char: str):
+        """ "Check if a chracter is a lowercase letter."""
+        return char.islower()
+
+    @staticmethod
+    def is_uppercase_letter(char: str):
+        """Check if a character is an uppercase letter."""
+        return char.isupper()
+
+    def countOfAtom(self, formula: str):
+        """Parse the number of atoms present in a simple
+        atom string, not containing any paranthesis."""
+
+    def countOfAtoms(self, formula: str) -> str:
+
+        return "placeholder"
+
+
+test_string = "H2O4Mg4Mgzj19A1"
+
+
+for i in test_string:
+    print(i)
