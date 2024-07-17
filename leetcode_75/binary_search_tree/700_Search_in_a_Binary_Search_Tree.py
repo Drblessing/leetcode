070af91eb6d2229b24eaf9b77f9279e403a06988
Danny Blessing <drblessing@gmail.com>
2024-07-17 12:56:36 -0400

    stack

--- leetcode_75/binary_search_tree/700_Search_in_a_Binary_Search_Tree.py
+++ leetcode_75/binary_search_tree/700_Search_in_a_Binary_Search_Tree.py
@@ -1,27 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        """Search a BST for a value."""
-        if not root:
-            return
+        """Search a BST for a value with short-circuiting."""
+        current = root
 
-        def _dfs(node, val):
-            if not node:
-                return
-
-            # Found it
-            if node.val == val:
-                return node
-
-            # Check left or right subtree based on value
-            if val < node.val:
-                return _dfs(node.left, val)
+        while current:
+            if current.val == val:
+                return current  # Found the value
+            elif val < current.val:
+                current = current.left  # Go left
             else:
-                return _dfs(node.right, val)
+                current = current.right  # Go right
 
-        return _dfs(root, val)
+        return None  # Value not found
