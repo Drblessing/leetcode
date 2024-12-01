89035fbcd245430839ebe0b6c649e4f77456d06e
Danny Blessing <drblessing@gmail.com>
2024-11-30 23:12:34 -0500

    Trying to fake a conflict

---
+++ Introduction to Pandas/2887. Fill Missing Data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    """Fill missing values in the 'quantity' column with 0."""
+    products["quantity"] = products["quantity"].fillna(0)
+    return products
+
+    # assign
+    # return products.assign(quantity=products["quantity"].fillna(0))
