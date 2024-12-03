adacd475b57468de6dddd18bbdda1e541fee8edf
Danny Blessing <drblessing@gmail.com>
2024-12-03 12:17:23 -0500

    features

--- Pandas/176. Second Highest Salary.py
+++ Pandas/176. Second Highest Salary.py
@@ -2,12 +2,17 @@ import pandas as pd
 
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    # Get unique salaries, sort in descending order, and fetch the second highest (if exists)
-    second_highest = (
-        employee["salary"].drop_duplicates().nlargest(2).iloc[-1]
-        if len(employee["salary"].drop_duplicates()) > 1
-        else None
-    )
+    """Get the second highest unique salary from the employee table.
+    If it does not exist, return a null dataframe."""
 
-    # Return as a DataFrame
-    return pd.DataFrame({"SecondHighestSalary": [second_highest]})
+    # Remove duplicates from the salary column
+
+    # # Get unique salaries, sort in descending order, and fetch the second highest (if exists)
+    # second_highest = (
+    #     employee["salary"].drop_duplicates().nlargest(2).iloc[-1]
+    #     if len(employee["salary"].drop_duplicates()) > 1
+    #     else None
+    # )
+
+    # # Return as a DataFrame
+    # return pd.DataFrame({"SecondHighestSalary": [second_highest]})
--- Pandas/177. Nth Highest Salary.py
+++ Pandas/177. Nth Highest Salary.py
@@ -2,9 +2,5 @@ import pandas as pd
 
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    sal = employee.salary.drop_duplicates().sort_values(ascending=False)
-    if len(sal) < N or N < 1:
-        x = None
-    else:
-        x = sal.iloc[N - 1]
-    return pd.DataFrame({f"getNthHighestSalary({N})": [x]})
+    """Return the nth_highest salary from the employee table, not counting duplicates.
+    If the nth highest salary does not exist, return a null dataframe."""
