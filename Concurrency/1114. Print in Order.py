77ce773155f5c8cbe7dc4368be2e736747d1a478
Danny Blessing <drblessing@gmail.com>
2024-12-08 20:47:26 -0500

    Fixed unnecessary bug.

---
+++ Concurrency/1114. Print in Order.py
@@ -0,0 +1,23 @@
+import threading
+
+
+class Foo:
+    def __init__(self):
+        self.first_done = threading.Event()
+        self.second_done = threading.Event()
+
+    def first(self, printFirst: "Callable[[], None]") -> None:
+        # printFirst() outputs "first"
+        printFirst()
+        self.first_done.set()
+
+    def second(self, printSecond: "Callable[[], None]") -> None:
+        self.first_done.wait()
+        # printSecond() outputs "second"
+        printSecond()
+        self.second_done.set()
+
+    def third(self, printThird: "Callable[[], None]") -> None:
+        self.second_done.wait()
+        # printThird() outputs "third"
+        printThird()
